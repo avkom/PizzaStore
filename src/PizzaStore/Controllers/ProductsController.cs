0f3b0e3146cf5fd957fad85a231ad5f703a9e6d9
avkom <qwerty@example.com>
2017-04-02 13:30:41 +0300

    Added ProductsController.cs.

---
+++ src/PizzaStore/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using PizzaStore.Models;
+
+namespace PizzaStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<ProductViewModel> products = new List<ProductViewModel>
+            {
+                new ProductViewModel
+                {
+                    Name = "Product 1 Name",
+                    Description = "Product 1 Description"
+                },
+                new ProductViewModel
+                {
+                    Name = "Product 2 Name",
+                    Description = "Product 2 Description"
+                },
+                new ProductViewModel
+                {
+                    Name = "Product 3 Name",
+                    Description = "Product 3 Description"
+                },
+                new ProductViewModel
+                {
+                    Name = "Product 4 Name",
+                    Description = "Product 4 Description"
+                },
+            };
+
+            return View("Products", products);
+        }
+    }
+}
---
+++ src/PizzaStore/Models/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PizzaStore.Models
+{
+    public class ProductViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
